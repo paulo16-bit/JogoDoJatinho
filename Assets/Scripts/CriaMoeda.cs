c1d6b8be708d699a1e8182c0d5f5f48546203d87
paulo16-bit <77926556+paulo16-bit@users.noreply.github.com>
2022-10-20 09:42:20 -0300

    Add files via upload

---
+++ Assets/Scripts/CriaMoeda.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class CriaMoeda : MonoBehaviour
+{
+    public GameObject moeda;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        CriaReta();
+        CriaReta2();
+        
+    }
+    void CriaReta(){
+        float y;
+        for (float x = -2; x < 3; x+=1.0f)
+        {            
+                // reta
+                y = 1.0f*x+0;
+                Instantiate(moeda, new Vector3(x,y,-2), Quaternion.identity);
+        }
+    }
+    void CriaReta2()
+    {
+        float y;
+        for (float x = -2; x < 3; x += 1.0f)
+        {
+            // reta
+            y = -1.0f * x -0;
+            Instantiate(moeda, new Vector3(x, y, -2), Quaternion.identity);
+        }
+    }
+    
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MoveAviao.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAviao : MonoBehaviour
+{
+    public GameObject aviao;
+    public float velocidade = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Método que é chamado quando a tela é atualizada
+    void Update()
+    {
+        
+        if(Input.GetKey(KeyCode.UpArrow)){
+            transform.Translate(new Vector3(0, Time.deltaTime * velocidade, 0));
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+            transform.Translate(new Vector3(0,-Time.deltaTime * velocidade, 0));
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            transform.Translate(new Vector3(-Time.deltaTime * velocidade, 0,0));
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            transform.Translate(new Vector3(Time.deltaTime*velocidade,0,0));
+        }
+
+    }
+}
